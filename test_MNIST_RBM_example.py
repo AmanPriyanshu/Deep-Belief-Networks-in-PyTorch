9389eb158a8d136f234b3109601b281a699aa012
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-01-08 02:36:33 +0530

    beautified images

--- test_MNIST_RBM_example.py
+++ test_MNIST_RBM_example.py
@@ -5,6 +5,35 @@ import os
 from RBM import RBM
 from MNIST_RBM_example import MNIST
 import cv2
+from PIL import Image
+from matplotlib import pyplot as plt
+
+def image_beautifier(names, final_name):
+
+	image_names = sorted(names)
+	images = [Image.open(x) for x in names]
+	widths, heights = zip(*(i.size for i in images))
+	total_width = sum(widths)
+	max_height = max(heights)
+	new_im = Image.new('RGB', (total_width, max_height))
+
+	x_offset = 0
+	for im in images:
+		new_im.paste(im, (x_offset,0))
+		x_offset += im.size[0]
+
+	new_im.save(final_name)
+	img = cv2.imread(final_name)
+	img = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
+	cv2.imwrite(final_name, img)
+
+def gen_displayable_images():
+	suffix = '_image.jpg'
+	for n in range(10):
+		prefix = './images_RBM/digitwise/'+str(n)+'_'
+		names = ['original', 'hidden', 'reconstructed']
+		names = [prefix+name+suffix for name in names]
+		image_beautifier(names, './images_RBM/'+str(n)+'.jpg')
 
 if __name__ == '__main__':
 	mnist = MNIST()
@@ -44,7 +73,23 @@ if __name__ == '__main__':
 		gen_image = gen_image.astype(np.int)
 
 		print(image.shape, hidden_image.shape, gen_image.shape)
+		
+		prefix = './images_RBM/digitwise/'+str(n)+'_'
+		suffix = '_image.jpg'
+		
+		plt.cla()
+		plt.imshow(image, cmap="gray")
+		plt.title('original image')
+		plt.savefig(prefix+'original'+suffix)
 
-		cv2.imwrite('./images_RBM/'+str(n)+'_original_image.jpg', image)
-		cv2.imwrite('./images_RBM/'+str(n)+'_hidden_image.jpg', hidden_image)
-		cv2.imwrite('./images_RBM/'+str(n)+'_reconstructed_image.jpg', gen_image)
+		plt.cla()
+		plt.imshow(hidden_image, cmap="gray")
+		plt.title('hidden image')
+		plt.savefig(prefix+'hidden'+suffix)
+
+		plt.cla()
+		plt.imshow(gen_image, cmap="gray")
+		plt.title('reconstructed image')
+		plt.savefig(prefix+'reconstructed'+suffix)
+
+	gen_displayable_images()
